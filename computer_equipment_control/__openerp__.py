d469c5e6612ec8e5aa7545b8d8aa8b667cd4a33c
Andy Rocio <andyrociogtz@gmail.com>
2015-03-29 16:57:31 -0600

    Agregado de descripcion __openerp__.py

---
+++ computer_equipment_control/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Computer Equipment Control",
+
+    'summary': """Equipment Controls""",
+
+
+    'author': "Andrea Rocio Gutierrez",
+    'website': "andyrociogtz@gmail.com",
+
+    'category': 'Generic Modules',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',],
+
+    # always loaded
+    'data': [
+         #'view/openacademy_course_view.xml',
+         #'view/openacademy_session_view.xml',
+         #'view/partner_view.xml',
+        #'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/openacademy_course_demo.xml',
+    ],
+    'installable':True,
