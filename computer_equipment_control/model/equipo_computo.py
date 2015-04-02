26195d639c6faf5de053e02be80c291d1cf420e4
Andy Rocio <andyrociogtz@gmail.com>
2015-04-02 16:16:15 -0600

    Estructura basica_guardada_dos

---
+++ computer_equipment_control/model/equipo_computo.py
@@ -0,0 +1,43 @@
+from openerp import api,models,fields, _
+
+
+"""
+This is a module that create the model of control
+"""
+
+class control(models.Model):
+    """
+    This class create a model class of control
+    """
+    _name = 'equipment.control' # String tomado por odoo 
+                                 #para crear tabla en postgres
+
+    name = fields.Char(string="Titulo", required=True) # Campo a generarse en la tabla _name
+    description = fields.Text()# campo de tipo Text para almacenar strings multilineas
+    responsible_id = fields.Many2one("res.users",
+                                    ondelete='set null',string="Responsable", index=True)
+    sessions_ids = fields.One2many('openacademy.session','course_id',string="Sessions")
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         _("The title of the course should not be the description")),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         _("The course title must be unique")),
+    ]
+
+    @api.one
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
